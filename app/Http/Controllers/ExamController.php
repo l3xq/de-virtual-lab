15b33efc5908c54f162a761c666aca5f615185f8
l3xq <cse.l3x@gmail.com>
2018-02-01 23:37:07 +0100

    Started debugging.

--- app/Http/Controllers/ExamController.php
+++ app/Http/Controllers/ExamController.php
@@ -12,34 +12,36 @@ class ExamController extends Controller
 {
     public function getExams(Request $request)
     {
-        $exams = json_encode(Exam::all()->toArray());
-        return Response::json(['status' => 200, 'data' => $exams]);
+        //$exams = json_encode(Exam::all()->toArray());
+
+        $exams = Exam::all()->toArray();
+        return response()->json(['status' => 200, 'data' => $exams]);
     }
 
     public function getExamById(Request $request, $id)
     {
-        $examById = json_encode(Exam::where('id', $id)->get()->toArray());
+        $examById = Exam::where('id', $id)->get()->toArray();
         return response()->json(['status' => 200, 'data' => $examById]);
     }
 
     public function getPeriodsByExamId(Request $request, $id)
     {
-        $periodsByExamId = json_encode(Period::where('exam_id', $id)->get()->toArray());
+        $periodsByExamId = Period::where('exam_id', $id)->get()->toArray();
         return response()->json(['status' => 200, 'data' => $periodsByExamId]);
     }
 
     public function getStudentsByExamAndPeriod(Request $request, $id, $pid)
     {
-        $studentsByExamAndPeriod = json_encode(Student::where([
+        $studentsByExamAndPeriod = Student::where([
             'exam_id' => $id,
             'period_id' => $pid,
-            ])->get()->toArray());
+            ])->get()->toArray();
         return response()->json(['status' => 200, 'data' => $studentsByExamAndPeriod]);
     }
 
     public function deleteExamById(Request $request, $id)
     {
-        $deleteExamById = json_encode(Exam::where('exam_id', $id)->delete());
+        $deleteExamById = Exam::where('exam_id', $id)->delete();
         return response()->json(['status' => 200, 'data' => 'Exam by ID[' . $id . '] has been deleted!']);
     }
 
@@ -48,6 +50,12 @@ class ExamController extends Controller
         $exam = new Exam;
         $exam->save();
 
+        return response()->json(['status' => 200, 'data' => 'New exam has been created.']);
+    }
+
+    public function updateExamById(Request $request, $id, Object $object)
+    {
+        dd($request);
         return response()->json(['status' => 200, 'data' => 'New exam has been created.']);
     }
 }
