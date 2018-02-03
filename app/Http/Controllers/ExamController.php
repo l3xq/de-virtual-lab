231d4a9967a225da96757198f14bdbea5f01f8cc
l3xq <cse.l3x@gmail.com>
2018-02-03 15:31:30 +0100

    fixed object

--- app/Http/Controllers/ExamController.php
+++ app/Http/Controllers/ExamController.php
@@ -58,7 +58,7 @@ class ExamController extends Controller
     {
         $exam = new Exam;
 
-        $exam->title = $_POST['title'];
+        $exam->title = $request['title'];
 
         $exam->save();
 
