7a625539a87b09dd8fe7a6a3fae23e01197d54c9
l3xq <cse.l3x@gmail.com>
2018-02-05 17:08:59 +0100

    notifications fix

--- app/Http/Controllers/NotificationController.php
+++ app/Http/Controllers/NotificationController.php
@@ -5,79 +5,53 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response as Response;
 use App\Notification;
-use Illuminate\Support\Facades\Log;
 
 class NotificationController extends Controller
 {
     public function getNotifications(Request $request)
     {
-//        try {
         $getNotifications = Notification::all()->toArray();
 
         return response()->json(['status' => 200, 'data' => $getNotifications]);
-        /*} catch (Exception $e) {
-            Log::error($e->getMessage());
-            continue;
-        }*/
     }
 
     public function getNotificationById(Request $request, $id)
     {
-        try {
-            $getNotification = Notification::where('id', $id)->get();
+        $getNotification = Notification::where('id', $id)->get();
 
-            return response()->json(['status' => 200, 'data' => $getNotification]);
-        } catch (Exception $e) {
-            Log::error($e->getMessage());
-            continue;
-        }
+        return response()->json(['status' => 200, 'data' => $getNotification]);
     }
 
     public function updateNotificationById(Request $request, $id)
     {
-        try {
-            $notification = Notification::find($id);
+        $notification = Notification::find($id);
 
-            $notification->title = $request['title'];
-            $notification->text = $request['text'];
-            $notification->time = $request['time'];
+        $notification->title = $request['title'];
+        $notification->text = $request['text'];
+        $notification->time = $request['time'];
 
-            $notification->save();
+        $notification->save();
 
-            return response()->json(['status' => 200, 'data' => 'Notification has been updated.']);
-        } catch (Exception $e) {
-            Log::error($e->getMessage());
-            continue;
-        }
+        return response()->json(['status' => 200, 'data' => 'Notification has been updated.']);
     }
 
     public function createNewNotification(Request $request)
     {
-        try {
-            $notification = new Notification;
+        $notification = new Notification;
 
-            $notification->title = $request['title'];
-            $notification->text = $request['text'];
-            $notification->time = $request['time'];
+        $notification->title = $request['title'];
+        $notification->text = $request['text'];
+        $notification->time = $request['time'];
 
-            $notification->save();
+        $notification->save();
 
-            return response()->json(['status' => 200, 'data' => 'New Notification has been created.']);
-        } catch (Exception $e) {
-            Log::error($e->getMessage());
-            continue;
-        }
+        return response()->json(['status' => 200, 'data' => 'New Notification has been created.']);
     }
 
     public function deleteNotificationById(Request $request, $id)
     {
-        try {
-            $deleteNotification = Notification::where('id', $id)->delete();
+        $deleteNotification = Notification::where('id', $id)->delete();
 
-            return response()->json(['status' => 200, 'data' => 'Notification by ID[' . $id . '] has been deleted!']);
-        } catch (Exception $e) {
-            Log::error($e->getMessage());
-            continue;
-        }
+        return response()->json(['status' => 200, 'data' => 'Notification by ID[' . $id . '] has been deleted!']);
     }
 }
