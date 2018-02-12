9b5de0d59821c258598132b5b13121ef19474e73
l3xq <cse.l3x@gmail.com>
2018-02-12 12:27:48 +0100

    Changed DB Schema. Fixed minor issues.

--- documentation/DB_BACKUPS/vlab_de_NEW_SCHEMA.sql
+++ documentation/DB_BACKUPS/vlab_de_NEW_SCHEMA.sql
@@ -1,11 +1,3 @@
--- phpMyAdmin SQL Dump
--- version 4.6.6deb5
--- https://www.phpmyadmin.net/
---
--- Host: localhost
--- Generation Time: Feb 03, 2018 at 10:29 PM
--- Server version: 5.7.21-0ubuntu0.17.10.1
--- PHP Version: 5.6.33-1+ubuntu17.10.1+deb.sury.org+1
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -81,17 +73,6 @@ CREATE TABLE IF NOT EXISTS `exams` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
--- Dumping data for table `exams`
---
-
-INSERT INTO `exams` (`title`, `created_at`, `updated_at`) VALUES
-('UPDATE EXAM 1', NULL, '2018-02-03 19:04:33'),
-('EXAM_2', NULL, NULL),
-('Exam_EXAMPLE', '2018-02-03 13:11:22', '2018-02-03 13:11:22');
-
--- --------------------------------------------------------
-
---
 -- Table structure for table `labs`
 --
 
@@ -104,16 +85,6 @@ CREATE TABLE IF NOT EXISTS `labs` (
 
   PRIMARY KEY(`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
---
--- Dumping data for table `labs`
---
-
-INSERT INTO `labs` (`title`, `link`, `created_at`, `updated_at`) VALUES
-('LAB_1', 'http://ftn.kg.ac.rs/', NULL, NULL),
-('LAB_2', 'http://ftn.kg.ac.rs/', NULL, NULL);
-
--- --------------------------------------------------------
 
 --
 -- Table structure for table `lessons`
@@ -134,47 +105,6 @@ CREATE TABLE IF NOT EXISTS `lessons` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
--- Dumping data for table `lessons`
---
-
--- INSERT INTO `lessons` (`title`, , `created_at`, `updated_at`, `exam_id`) VALUES
--- ('LESSON_1', 'http://ftn.kg.ac.rs/', NULL, NULL, 1),
--- ('RANDOM TITLE', 'ftn.kg.ac.rs', NULL, '2018-02-03 19:07:38', 2),
--- ('LESSON_3', 'http://ftn.kg.ac.rs/', NULL, NULL, 2),
--- ('LESSON_4', 'http://ftn.kg.ac.rs/', NULL, NULL, 1),
--- ('LESSON_CREATED', 'PATH_FOR_NEW_LESSON', '2018-02-03 19:12:34', '2018-02-03 19:12:34', 2);
-
--- --------------------------------------------------------
-
---
--- Table structure for table `migrations`
---
-
-CREATE TABLE IF NOT EXISTS `migrations` (
-  `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
-  `migration` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `batch` int(11) NOT NULL,
-
-  PRIMARY KEY(`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
---
--- Dumping data for table `migrations`
---
-
-INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
-(1, '2014_10_12_000000_create_users_table', 1),
-(2, '2014_10_12_100000_create_password_resets_table', 1),
-(3, '2018_02_01_111524_create_exams_table', 1),
-(4, '2018_02_01_111638_create_students_table', 1),
-(5, '2018_02_01_111720_create_notifications_table', 1),
-(6, '2018_02_01_111826_create_lessons_table', 1),
-(7, '2018_02_01_112026_create_labs_table', 1),
-(8, '2018_02_01_112123_create_periods_table', 1);
-
--- --------------------------------------------------------
-
---
 -- Table structure for table `notifications`
 --
 
@@ -190,29 +120,6 @@ CREATE TABLE IF NOT EXISTS `notifications` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
--- Dumping data for table `notifications`
---
-
-INSERT INTO `notifications` (`title`, `text`, `time`, `created_at`, `updated_at`) VALUES
-('RANDOM TITLE', 'LOREM IPSUMNMSMSMSMS', 159562354, NULL, '2018-02-03 20:10:45'),
-('NOTIFICATION_3', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc sit amet nibh quis urna rhoncus aliquam id id ante. Vestibulum vel placerat elit, non tincidunt massa. Praesent rhoncus ut felis ultrices ultricies. Pellentesque efficitur dignissim libero quis luctus. Ut ac leo egestas, pellentesque purus sed, finibus ipsum. In et eros vitae eros venenatis luctus. Vivamus eget lacus in nunc porttitor blandit. Integer eget sem justo. Aenean non magna eget lectus luctus finibus hendrerit vitae nulla. Nunc ut gravida enim. Integer efficitur mi non rhoncus tristique. Aliquam erat volutpat. Morbi lectus justo, sagittis vitae ultrices nec, eleifend vitae nisi.', 1517483960, NULL, NULL),
-('RANDOM TITLE', 'LOREM IPSUMNMSMSMSMS', 159562354, '2018-02-03 20:13:46', '2018-02-03 20:13:46');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `password_resets`
---
-
-CREATE TABLE IF NOT EXISTS `password_resets` (
-  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `token` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `created_at` timestamp NULL DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
--- --------------------------------------------------------
-
---
 -- Table structure for table `periods`
 --
 
@@ -225,19 +132,6 @@ CREATE TABLE IF NOT EXISTS `periods` (
 
   PRIMARY KEY(`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
---
--- Dumping data for table `periods`
---
-
-INSERT INTO `periods` (`name`, `created_at`, `updated_at`, `exam_id`) VALUES
-('PERIOD_1', NULL, NULL, 1),
-('NAME OF PERIOD', NULL, '2018-02-03 19:29:27', 1),
-('PERIOD_3', NULL, NULL, 1),
-('PERIOD_4', NULL, NULL, 2),
-('NEW PERIOD', '2018-02-03 19:31:12', '2018-02-03 19:31:12', 3);
-
--- --------------------------------------------------------
 
 --
 -- Table structure for table `students`
@@ -259,34 +153,6 @@ CREATE TABLE IF NOT EXISTS `students` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
--- Dumping data for table `students`
---
-
-INSERT INTO `students` (`firstName`, `lastName`, `index`, `mark`, `unit`, `created_at`, `updated_at`, `exam_id`, `period_id`) VALUES
-('Marko', 'Tomic', '21/2011', 9.00, 'RI', NULL, NULL, 1, 2),
-('Alex', 'Nesh', '21/2121', 10.99, 'Envoy', NULL, '2018-02-03 19:25:16', 1, 3),
-('Serhio', 'Satanini', '100/2011', 6.66, 'RI', NULL, NULL, 2, 3),
-('Alex', 'Nesh', '21/2121', 10.99, 'Envoy', '2018-02-03 19:28:05', '2018-02-03 19:28:05', 2, 2);
-
--- --------------------------------------------------------
-
---
--- Table structure for table `users`
---
-
-CREATE TABLE IF NOT EXISTS `users` (
-  `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `remember_token` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `created_at` timestamp NULL DEFAULT NULL,
-  `updated_at` timestamp NULL DEFAULT NULL,
-
-  PRIMARY KEY(`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
---
 -- Indexes for dumped tables
 --
 
@@ -295,12 +161,6 @@ CREATE TABLE IF NOT EXISTS `users` (
 --
 ALTER TABLE `lessons`
   ADD KEY `exam_id` (`exam_id`);
-
---
--- Indexes for table `password_resets`
---
-ALTER TABLE `password_resets`
-  ADD KEY `password_resets_email_index` (`email`);
 
 --
 -- Indexes for table `periods`
@@ -314,17 +174,6 @@ ALTER TABLE `periods`
 ALTER TABLE `students`
   ADD KEY `exam_id` (`exam_id`),
   ADD KEY `period_id` (`period_id`);
-
---
--- Indexes for table `users`
---
-ALTER TABLE `users`
-  ADD UNIQUE KEY `users_email_unique` (`email`);
-
-
---
--- Constraints for dumped tables
---
 
 --
 -- Constraints for table `lessons`
